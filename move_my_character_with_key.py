6e4b966993a43ef24f34b6ae95a1802da2fc0176
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-09-25 16:58:10 +0900

    기본 뼈대 생성

---
+++ move_my_character_with_key.py
@@ -0,0 +1,15 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+
+character = load_image('animation_sheet.png')
+
+running = True
+frame = 0
+
+while running:
+    pass
+
+close_canvas()
