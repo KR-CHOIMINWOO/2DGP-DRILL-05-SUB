b0b1656a4b4077a9a1c002a59ab8e580945e0326
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-09-25 21:20:35 +0900

    방향을 정해주는 함수 뼈대 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,8 +5,11 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def update_direction():
+    global z
+
 def handle_events():
-    global running , dir_x, dir_y, z
+    global running , dir_x, dir_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
