80801c536de8c6f6e339dd53fcdd3f332882dd0e
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-09-25 17:04:17 +0900

    핸들 이벤트 함수 뼈대 추가 및 글로벌 변수 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,10 +1,16 @@
 from pico2d import *
 
+from move_character_with_mouse import handle_events
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
-
 character = load_image('animation_sheet.png')
+
+def handle_events():
+    global running , x, y, z
+
+    pass
 
 running = True
 frame = 0
@@ -13,9 +19,10 @@ while running:
     clear_canvas()
 
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * z, 100, 100, x, y)
 
     update_canvas()
+    handle_events()
     frame = (frame + 1) % 8
     delay(0.05)
     pass
